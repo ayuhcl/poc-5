3bc9709842f8cba7232f4e404fb2033bba8d2095
ayuhcl <ayushi-agarwal@hcltech.com>
2026-06-11 10:58:41 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -3,7 +3,7 @@ module "eks" {
   version         = "~> 20.0"
 
   cluster_name    = "poc-5-eks"
-  cluster_version = "1.27"
+  cluster_version = "1.29"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
